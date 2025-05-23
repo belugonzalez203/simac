f6f50cf9c034215b27e1de358780924865074920
belugonzalez203 <belen.gonzalez0724@jala.university>
2025-05-23 01:42:00 -0400

    Initial structure

---
+++ simac-backend/src/main/java/com/simac/SimacBackendApplication.java
@@ -0,0 +1,13 @@
+package com.simac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimacBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimacBackendApplication.class, args);
+    }
+
+}
